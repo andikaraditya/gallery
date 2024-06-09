fc11f5b6670891321dee0f738e15e39c0d1fe47d
andika <andikaraditya1@gmail.com>
2024-06-09 13:12:27 +0700

    add item detail page

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,39 @@
+import fs from "fs"
+
+function getgalleryContent(slug: string) {
+  const imgFolder = "./img/"
+  const src = `${imgFolder}${slug}.jpg`
+  return {
+    src,
+  }
+}
+
+function ItemPage(props: any) {
+  const slug = props.params.slug
+  const data = getgalleryContent(slug)
+  return (
+    <main className="">
+      <div className="w-[95%] mx-auto border-b-4 border-slate-900 py-11 mb-12">
+        <h1 className="text-4xl md:text-6xl font-bold text-slate-900 text-center dark:text-gray-300 capitalize">
+          Lightstation Gallery
+        </h1>
+      </div>
+      <div className="w-[95%] mx-auto flex flex-col 2xl:flex-row gap-8 items-start justify-start">
+        <img className="min-w-[40%] mx-auto" src={data.src} alt="" />
+        <div>
+          <h2 className="text-6xl text-center mb-7 font-bold capitalize">{slug}</h2>
+          <p className="text-3xl text-justify">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur eligendi, corrupti
+            cupiditate et enim, porro officiis ratione eos exercitationem quas earum maxime
+            recusandae quia commodi qui dicta magni, illo delectus. Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Delectus ipsa libero dolorem exercitationem labore quae
+            aspernatur fugit inventore? Pariatur quidem, illo repellendus iusto dolorem ratione
+            repellat quis veniam facere illum.
+          </p>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default ItemPage
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import fs from "fs"
+import Link from "next/link"
 
 type ImageGallery = {
   title: string
@@ -19,7 +20,13 @@ export default function Home() {
       <div className="md:columns-2 md:gap-6 md:space-y-6 lg:columns-4 lg:gap-8 lg:space-y-8 w-[95%] mx-auto">
         {galery.map((el, index) => {
           return (
-            <ImageItem src={el.src} title={el.title} description={el.description} key={index} />
+            <ImageItem
+              src={el.src}
+              title={el.title}
+              description={el.description}
+              slug={el.slug}
+              key={index}
+            />
           )
         })}
       </div>
@@ -31,18 +38,22 @@ function ImageItem({
   src,
   title,
   description,
+  slug,
 }: {
   src: string
   title: string
   description: string
+  slug: string
 }) {
   return (
     <div className="group hover:cursor-pointer relative rounded-2xl overflow-hidden">
-      <img className="w-full" src={src} alt="" />
-      <div className="group-hover:block group-hover:absolute ease-in-out hidden bottom-0 p-3 bg-slate-900/50 backdrop-blur-[1px]">
-        <h2 className="text-2xl text-white capitalize font-bold">{title}</h2>
-        <p className="line-clamp-3 text-white">{description}</p>
-      </div>
+      <Link href={slug}>
+        <img className="w-full" src={src} alt="" />
+        <div className="group-hover:block group-hover:absolute ease-in-out hidden bottom-0 p-3 bg-slate-900/50 backdrop-blur-[1px]">
+          <h2 className="text-2xl text-white capitalize font-bold">{title}</h2>
+          <p className="line-clamp-3 text-white">{description}</p>
+        </div>
+      </Link>
     </div>
   )
 }
